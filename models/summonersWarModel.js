ccdc40d9dd3f26c842d53aad7d9f00f32be6a080
Rémy Le Foll <remy.le-foll@universite-paris-saclay.fr>
2021-07-23 23:59:35 +0200

    Add runes model

--- models/summonersWarModel.js
+++ models/summonersWarModel.js
@@ -1,12 +1,39 @@
 import mongoose from 'mongoose'
 
-const summonersWarSchema = new mongoose.Schema({
-    rune:{
-        name:String,
-        type:[String],
-        information:{
-            propriete:[String],
-
-        }
-    }
+const summonersWarSchemaRune = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    antique: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    propriete: {
+        type: new mongoose.Schema({
+            star: {
+                type: Number,
+                required: true,
+                unique: true
+            },
+            stat: {
+                type: String,
+                required: true,
+                uppercase: true
+            },
+            subStat: {
+                type: String,
+                required: true,
+                uppercase: true
+            },
+            extremum: {
+                type: [Number],
+                unique: true,
+                required: true
+            }
+        })
+    },
+    
 })
